7ee9af87fb693ce665b6e88bc3e12f052181c771
lizziedelmonico <123976957+lizziedelmonico@users.noreply.github.com>
2023-11-05 10:38:39 -0500

    *toad voice* WAAAAAAHHH

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,3 +1,6 @@
+import java.util.Collections;
+import java.util.ListIterator;
+
 public class InsertionSort {
   
   public static CardPile sort(CardPile unsorted, SortRecorder record) {
@@ -7,16 +10,48 @@ public class InsertionSort {
 
     // Here is the result list you will be creating
     CardPile sorted = new CardPile();
-  
-    // ***********************************************************
-    // Here is where you'll do the "work" of InsertionSort:
-    //   - Use sorted to store the "sorted portion"
-    //   - Don't forget to register the new state with the
-    //     recorder after each card is transferred:
-
-    // ***********************************************************
-
+    Card current = unsorted.getFirst();
+    
+    
+    while(!unsorted.isEmpty()){
+      for(ListIterator<Card> position = unsorted.listIterator(); position.hasNext();){
+        Card temp = position.next();
+        sorted.add(current);
+        current = temp;
+      }
+    }
+    
+    
     // return the sorted result here
     return sorted;
+  }
+
+    public static void main(String args[]) {
+
+    // set up a class to record and display the sorting results
+    SortRecorder recorder = new SortRecorder();
+
+    // set up the deck of cards
+    Card.loadImages(recorder);
+    CardPile cards = new CardPile(Card.newDeck(true), 2, 2);
+
+    // for debugging purposes, uncomment this to
+    // work with a smaller number of cards:
+    // cards = cards.split(cards.get(39));
+
+    // mix up the cards
+    Collections.shuffle(cards);
+
+    // if you want to sort in array form, use:
+    Card[] card_arr = cards.toArray(new Card[0]);
+
+    // in your program, this would be a call to a real sorting algorithm
+    cards = FakeSort.sort(cards, recorder);
+
+    // We can print out the (un)sorted result:
+    System.out.println(cards);
+
+    // make window appear showing the record
+    recorder.display("Card Sort Demo: FakeSort");
   }
 }
--- SelectionSort.java
+++ SelectionSort.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class SelectionSort {
   
   public static CardPile sort(CardPile unsorted, SortRecorder record) {
@@ -8,18 +10,26 @@ public class SelectionSort {
     // Here is the result list you will be creating
     CardPile sorted = new CardPile();
   
-    // ***********************************************************
-    // Here is where you'll do the "work" of SelectionSort:
-    //   - Use sorted to store the "sorted portion"
-    //   - Don't forget to register the new state with the
-    //     recorder after each card is transferred:
-    //        record.next();        // tell it this is a new step
-    //        record.add(sorted);   // the sorted pile
-    //        record.add(unsorted); // the unsorted pile
-    // ***********************************************************
+    Card min = unsorted.getFirst();
+    int index = 0;
+
+    while(!unsorted.isEmpty()){
+      for(ListIterator<Card> position = unsorted.listIterator(); position.hasNext();){
+        Card c = position.next();
+        if(c.compareTo(min) < 0){
+          min = c;
+          index = position.nextIndex();
+        } 
+        unsorted.remove(min);
+        sorted.add(min);
+      }
+
+  
+
+    }
 
     // return the sorted result here
-    System.out.println("bababoey");
+    System.out.println(sorted);
     return sorted;
   
   }
